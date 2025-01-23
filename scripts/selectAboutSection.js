ac117d258164bab4981978e8f988df9d348512c2
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-01-23 01:06:29 -0800

    general updates
    - compress images and place them in the original folder
    - move high-resolution images to their own, separate folder
    - edit about me/site sections
    - remove todo file, list is now directly in HTML
    - update css for scrolling, content sizing, photo gallery
    - continue WIP of mobile site design

--- scripts/selectAboutSection.js
+++ scripts/selectAboutSection.js
@@ -11,17 +11,36 @@ const yearsSinceHighSchool = () => {
     return (yearWords[n]);
 }
 
-const aboutMeText = `Hi! I'm Anton - the developer of this site! <br>
-I'm a third-year student at Simon Fraser University, in Vancouver, B.C., completing my BSc in Computing Science. <br>
-I have been working as a full-stack web developer for the past ${yearsSinceHighSchool()} years, starting in my twelfth grade of high school. <br>
-In addition to English, I speak fluent Russian, having spoken it at home my entire life, and French, from participating in the French Immersion Program throughout my entire K-12 schooling. <br>
-I have a passion for anything old and outdated, especially anything from the Y2K era, like classic iPods, digicams, and CDs, which I heavily use as inspiration for my work. <br>
-I am a member of <a href="https://sfusurge.com/">SFU Surge</a>, SFU's largest student-led tech-focused organisation, where I have held the position of photographer/social media executive since September 2024.`
-
-const aboutSiteText = `This site is heavily inspired by websites from the <a href="https://www.webdesignmuseum.org/golden-age-of-web-design">Golden Age Of Web Design</a>. <br> 
-I chose it due to its uniqueness and recognisability - even those who didn't grow up on those Adobe Flash-based sites will immediately recognise it's sharp angles, metallic sheen and techno-futurism. <br>
-However, this approach is not just an exercise in creativity - there exists a practical side to it, too. <br>
-Just like students learn their country's history in school, I believe that developers have a duty to learn about the past of their own world - in order to truly understand current trends and technologies, we must educate ourselves about those of the past, and learn from their sucesses and failures. <br>
+const aboutMeText = `Hi! I'm Anton - the developer of this site!
+<br>
+I'm a third-year student at Simon Fraser University, in Vancouver, B.C., studying Computing Science.
+<br>
+I've been working as a full-stack web developer since 2021, with my main focus being React-based websites,
+including single-page invoice generators, concert ticketing websites and everything in between.
+<br>
+In addition to studying at SFU, I am an active member of <a href="https://sfusurge.com/">SFU Surge</a>, my university's
+largest student-led tech-focused organisation, where I have held the position of photographer/social media executive
+since September 2024.
+<br>
+I spend my free time listening to music (I'm particularly fond of
+<a href="https://www.youtube.com/watch?v=ap0mqwvf7H0">Taking Back Sunday</a> and
+<a href="https://www.youtube.com/watch?v=PoTu269qSnw">Porter Robinson</a>), and tinkering with technology.
+From engines to film cameras - if I can get my hands on it, chances are that it will be taken apart at some point.
+`
+const aboutSiteText = `This site is heavily inspired by websites from the <a href="https://www.webdesignmuseum.org/golden-age-of-web-design">Golden Age Of Web Design</a>.
+<br>
+I chose it due to its uniqueness and recognizability - even those who didn't grow up on those Adobe Flash-based sites will
+immediately catch it's sharp angles, metallic sheen and techno-futurism.
+<br>
+The inspiration isn't just surface-level - those who grew up browsing Angelfire and Geocities sites will recognize the
+<a href="https://en.wikipedia.org/wiki/Matrix_digital_rain">Matrix</a>/<a href="https://lain.wiki/images/e/ee/Metaphorlize.jpg">Lain</a>
+-like scrolling text, and the ever-ubiquitous snow that covered the web every December in 200X.
+<br>
+However, this approach isn't just an exercise in creativity - there exists a practical side to it, too.
+<br>
+Just like students learn their country's history in school, I believe that developers have a duty to learn about the
+past of their world - in order to truly understand current trends and technologies, we must educate ourselves about
+those of the past, and learn from their successes and failures. <br>
 <br>
 <marquee>Anyone remember the &lt;marquee&gt; tag?</marquee>
 `
