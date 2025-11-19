77fec13776d6d00d83a87cfa1c1aac4a396e3954
Anton-Pan <91840673+Anton-Pan@users.noreply.github.com>
2025-11-19 01:44:22 -0800

    Wrote a script to automatically fetch when the project was last updated from GitHub - Minor wording fixed to the About Me and Projects sections - Minor mobile-site CSS bugfixes - Replaced all '0' CSS sizes with '0rem' for easier reading + editability

---
+++ scripts/set-last-update-date.js
@@ -0,0 +1,31 @@
+const setLastUpdateDate = async () => {
+    const textDiv = document.getElementById("lastUpdateDate");
+    if (!textDiv) {
+        return;
+    }
+
+    await fetch("https://api.github.com/repos/Anton-Pan/Anton-Pan.github.io")
+
+        .then(res => {
+            if (!res.ok) {
+                throw res;
+            }
+            return res.json();
+        })
+        .then(data => {
+                console.log(data);
+                const lastUpdateDate = new Date(data.updated_at); // eg: "2025-11-19T08:48:04Z"
+                if (!textDiv || !lastUpdateDate) {
+                    return;
+                }
+                const updateDay = lastUpdateDate.getDate()
+                const updateMonth = lastUpdateDate.toLocaleString('default', {month: 'long'});
+                const updateYear = lastUpdateDate.getFullYear();
+                textDiv.innerText = `last update: ${updateMonth} ${updateDay}, ${updateYear}`; // eg: "last update: November 1st, 2025"
+            }
+        )
+}
+
+(document.addEventListener("DOMContentLoaded", async () => {
+    await setLastUpdateDate();
+}));
